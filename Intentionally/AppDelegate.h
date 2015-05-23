c0b3a4beec3caaaf8254ea5b99f70311435fa81b
Garrett Disco <garrettdisco@gmail.com>
2015-05-22 21:38:16 -0500

    Initial Commit

---
+++ Intentionally/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Intentionally
+//
+//  Created by Garrett Disco on 5/22/15.
+//  Copyright (c) 2015 Garrett Disco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Intentionally/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Intentionally
+//
+//  Created by Garrett Disco on 5/22/15.
+//  Copyright (c) 2015 Garrett Disco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
